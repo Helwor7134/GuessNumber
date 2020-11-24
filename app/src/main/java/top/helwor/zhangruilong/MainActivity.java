6535e8345cb29bfced15e1b9ea72ccd09200e1c4
helwor <2992845224@qq.com>
2020-11-24 09:34:14 +0800

    complete

---
+++ app/src/main/java/top/helwor/zhangruilong/MainActivity.java
@@ -0,0 +1,24 @@
+package top.helwor.zhangruilong;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+    private Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.main_btn_start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateTo(StartGame.class);
+            }
+        });
+    }
+}
